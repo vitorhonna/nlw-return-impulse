35e5c10c3166ca7626803394bc3ac5e21a0db2de
Vitor Honna <vitorhonna@gmail.com>
2022-05-03 10:30:25 -0300

    add feedback types

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -13,7 +13,7 @@ export function Widget() {
             <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
                 <ChatTeardropDots className="w-6 h-6" />
 
-                <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+                <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear select-none">
                     <span className="pl-2"></span>
                     Feedback
                 </span>
