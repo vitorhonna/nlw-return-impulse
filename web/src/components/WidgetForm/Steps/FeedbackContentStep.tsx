9adcc3a81216ddb97411165ebc7cb73ebbc53c48
Vitor Honna <vitorhonna@gmail.com>
2022-05-03 17:26:50 -0300

    add loading and screenshot logic

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,6 +1,8 @@
 import { FeedbackType, feedbackTypes } from '..';
 import { CloseButton } from '../../CloseButton';
 import { ArrowLeft, Camera } from 'phosphor-react';
+import { ScreenshotButton } from './ScreenshotButton';
+import { useState } from 'react';
 
 interface FeedbackContentStepProps {
     feedbackType: FeedbackType;
@@ -11,6 +13,8 @@ export function FeedbackContentStep({
     feedbackType,
     onFeedbackRestartRequested
 }: FeedbackContentStepProps) {
+    const [screenshot, setScreenshot] = useState<string | null>(null);
+
     const feedbackTypeInfo = feedbackTypes[feedbackType];
 
     return (
@@ -39,12 +43,9 @@ export function FeedbackContentStep({
                 />
 
                 <footer className="flex gap-2 mt-2">
-                    <button
-                        type="button"
-                        className="p-2 bg-zinc-800 rounded-md border-transparent hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
-                    >
-                        <Camera className="w-6 h-6 text-zinc-100" />
-                    </button>
+                    <ScreenshotButton
+                        onScreenshotTook={setScreenshot}
+                    />
 
                     <button
                         type="submit"
