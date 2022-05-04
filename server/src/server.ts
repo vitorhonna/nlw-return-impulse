07858fd7d715089138f7596175a430c84717f113
Vitor Honna <vitorhonna@gmail.com>
2022-05-04 13:22:16 -0300

    apply dependency inversion principle

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,48 +1,11 @@
 import express from 'express'
-import nodemailer from 'nodemailer'
-import { prisma } from './prisma';
+import { routes } from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-const transport = nodemailer.createTransport({
-    host: "smtp.mailtrap.io",
-    port: 2525,
-    auth: {
-        user: "785540d7d95746",
-        pass: "0294c121b3c3ab"
-    }
-});
-
-app.post('/feedbacks', async (req, res) => {
-    const { type, comment, screenshot } = req.body;
-
-    const feedback = await prisma.feedback.create({
-        data: {
-            type,
-            comment,
-            screenshot,
-        }
-    })
-
-    await transport.sendMail({
-        from: 'Equipe Feedget <oi@feedget.com>',
-        to: 'Vitor Honna <vitorhonna@gmail.com>',
-        subject: 'Novo feedback',
-        html: [
-            `<div style="font-family: sans-serif; font-size: 16px; color:#111">`,
-            `<p>Feedback</p>`,
-            `<p>Tipo: ${type}</p>`,
-            `<p>Comentário: ${comment}</p>`,
-            // `<p>Screenshot: ${screenshot}`,
-            `</div>`
-        ].join('\n')
-    })
-
-    return res.status(201).json({ data: feedback })
-})
+app.use(routes);
 
 app.listen(3333, () => {
     console.log('HTTP server running!')
-})
+});
