7b56cb02b9d37447322e7cab3428258de62b5ba2
Vitor Honna <vitorhonna@gmail.com>
2022-05-09 15:28:09 -0300

    add PORT env variable

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,6 +8,6 @@ app.use(cors()); // Determina quem pode consumir a API (neste caso está aberto 
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
     console.log('HTTP server running!')
 });
